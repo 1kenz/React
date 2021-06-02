dce5ab4aea620e414c7fd60c77c0a8afff6870cb
xkendx <63884610+xkendx@users.noreply.github.com>
2021-06-02 22:58:22 +0300

    feat: update

---
+++ src/app.js
@@ -0,0 +1,34 @@
+// var, let, const
+// var ==> pure javascript
+// file based
+// JS is not a type safe language
+// object notations
+
+var number = 7;
+let number2 = 35;
+number2 = 28;
+
+let students = { id: 1, name: 'Kenan' };
+
+console.log('number is ' + number);
+console.log('number is ' + number2);
+console.log(students);
+
+function save(student, note = 100) {
+    console.log(student.name + ': ' + note);
+    // console.log('Saved');
+}
+
+function save2(note = 100, student) {
+    console.log(student.name + ': ' + note);
+    // console.log('Saved');
+}
+save(students, 55);
+save2(undefined, students); // default use
+// save(15);
+
+let newArrays = ['Kenan', 'Ayşegül'];
+console.log(newArrays);
+
+let newArrays2 = [newArrays, students, 'İstanbul', { city: 'Samsun' }];
+console.log(newArrays2);
