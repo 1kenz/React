6f56ee680a3c8a7b541e6e583554953d1f9e2381
xkendx <63884610+xkendx@users.noreply.github.com>
2021-06-05 17:47:32 +0300

    feat: js fundamentals

---
+++ src/fundamentals/index.js
@@ -0,0 +1,20 @@
+//! DATA TYPES
+// String
+// Number
+// Boolean
+// Array
+// Object
+// not a type safe language
+
+// var student = 'Kenan Deniz';
+// let value = 7;
+// console.log(student);
+// console.log(value);
+
+var students = ['Kenan', 'Ayşegül', 'Kerem'];
+console.log(students);
+console.log(students[1]);
+students.push('Ahmet');
+console.log(students);
+students[students.length] = 'Hüseyin';
+console.log(students);
