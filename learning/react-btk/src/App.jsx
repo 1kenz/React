abd2283a5c7dd3211630eea699b15214100342a6
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-01 02:07:48 +0300

    feat: state management with pure react

--- learning/react-btk/src/App.jsx
+++ learning/react-btk/src/App.jsx
@@ -3,28 +3,44 @@ import ProductList from './ProductList';
 import CategoryList from './CategoryList';
 import { Row, Container, Col } from 'reactstrap';
 
-function App() {
-    // encapsulation
-    let categoryInfo = { title: 'Category List' };
-    let productInfo = { title: 'Product List' };
+import React, { Component } from 'react';
 
-    return (
-        <div>
-            <Container>
-                <Row>
-                    <Navi />
-                </Row>
-                <Row>
-                    <Col xs="3">
-                        <CategoryList info={categoryInfo} />
-                    </Col>
-                    <Col xs="9">
-                        <ProductList info={productInfo} />
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+export default class App extends Component {
+    state = { currentCategory: '' };
+
+    changeCategory = (category) => {
+        this.setState({
+            currentCategory: category.categoryName,
+        });
+    };
+
+    render() {
+        // encapsulation
+        let categoryInfo = { title: 'Category List' };
+        let productInfo = { title: 'Product List' };
+        return (
+            <div>
+                <Container>
+                    <Row>
+                        <Navi />
+                    </Row>
+                    <Row>
+                        <Col xs="3">
+                            <CategoryList
+                                currentCategory={this.state.currentCategory}
+                                changeCategory={this.changeCategory}
+                                info={categoryInfo}
+                            />
+                        </Col>
+                        <Col xs="9">
+                            <ProductList
+                                currentCategory={this.state.currentCategory}
+                                info={productInfo}
+                            />
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
 }
-
-export default App;
--- learning/react-btk/src/ProductList.jsx
+++ learning/react-btk/src/ProductList.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 export default function ProductList(props) {
     let title = props.info.title;
+    let currentCategory = props.currentCategory;
 
     return (
         <div>
             <h2>{title}</h2>
+            <h3>{currentCategory}</h3>
         </div>
     );
 }
