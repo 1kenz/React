dd03fae92c3211e2d53e305bcf230e6023c50205
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-01 01:33:29 +0300

    feat: create categories mapping with constructor

--- learning/react-btk/src/CategoryList.jsx
+++ learning/react-btk/src/CategoryList.jsx
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
 export default class CategoryList extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = [{},{},{}]
-    // }
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            categories: [
+                { categoryId: 1, categoryName: 'Beverages' },
+                { categoryId: 2, categoryName: 'Condiments' },
+            ],
+        };
+    }
     render() {
         let title = this.props.info.title;
 
@@ -14,11 +18,11 @@ export default class CategoryList extends Component {
             <div>
                 <h2>{title}</h2>
                 <ListGroup>
-                    <ListGroupItem>Cras justo odio</ListGroupItem>
-                    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                    <ListGroupItem>Morbi leo risus</ListGroupItem>
-                    <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-                    <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                    {this.state.categories.map((category) => (
+                        <ListGroupItem key={category.categoryId}>
+                            {category.categoryName}
+                        </ListGroupItem>
+                    ))}
                 </ListGroup>
             </div>
         );
