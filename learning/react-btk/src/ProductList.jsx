05f8c29e15a3744c9b26b8c4e6fb915f0ec9acc0
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-01 00:43:54 +0300

    feat: use props

--- learning/react-btk/src/ProductList.jsx
+++ learning/react-btk/src/ProductList.jsx
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 export default class ProductList extends Component {
     constructor(props) {
         super(props);
-        var title = this.props.title;
+        // var title = this.props.title;
     }
 
     render() {
         return (
             <div>
-                <h2>{title}</h2>
+                <h2>{this.props.title}</h2>
             </div>
         );
     }
