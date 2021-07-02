c62ae94d1dfb1f59acdeea87b6c5e287daff5ad9
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-02 04:21:12 +0300

    feat: add event.preventDefault for no render all component

--- learning/react-btk/src/FormDemo1.jsx
+++ learning/react-btk/src/FormDemo1.jsx
@@ -7,13 +7,19 @@ export default class FormDemo1 extends Component {
         this.setState({ userName: event.target.value });
     };
 
+    onSubmitHandler = (event) => {
+        event.preventDefault();
+        alert(this.state.userName);
+    };
+
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.onSubmitHandler}>
                     <h2>User Name</h2>
                     <input type="text" onChange={this.onChangeHandler} />
                     <h3>User name is {this.state.userName}</h3>
+                    <input type="submit" value="Save" />
                 </form>
             </div>
         );
