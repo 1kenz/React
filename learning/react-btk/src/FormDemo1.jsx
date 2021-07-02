b0f297b80bae321a2ac45e95a67bd97728c7c643
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-02 04:29:29 +0300

    feat: multi input

--- learning/react-btk/src/FormDemo1.jsx
+++ learning/react-btk/src/FormDemo1.jsx
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 
 export default class FormDemo1 extends Component {
-    state = { userName: '' };
+    state = { userName: '', city: '' };
 
     onChangeHandler = (event) => {
-        this.setState({ userName: event.target.value });
+        // this.setState({ userName: event.target.value });
+
+        let name = event.target.name;
+        let value = event.target.value;
+
+        this.setState({ [name]: value });
     };
 
     onSubmitHandler = (event) => {
         event.preventDefault();
         alert(this.state.userName);
+        alert(this.state.city);
     };
 
     render() {
@@ -17,8 +23,21 @@ export default class FormDemo1 extends Component {
             <div>
                 <form onSubmit={this.onSubmitHandler}>
                     <h2>User Name</h2>
-                    <input type="text" onChange={this.onChangeHandler} />
+                    <input
+                        name="userName"
+                        type="text"
+                        onChange={this.onChangeHandler}
+                    />
                     <h3>User name is {this.state.userName}</h3>
+
+                    <h2>City</h2>
+                    <input
+                        name="city"
+                        type="text"
+                        onChange={this.onChangeHandler}
+                    />
+                    <h3>City is {this.state.city}</h3>
+
                     <input type="submit" value="Save" />
                 </form>
             </div>
