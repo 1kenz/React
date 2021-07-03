71c8ba7bfd420cc67ef0232ddab63a8fef1aedc9
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-03 22:53:31 +0300

    feat: create product listing

--- learning/redux-northwind/src/components/categories/CategoryList.jsx
+++ learning/redux-northwind/src/components/categories/CategoryList.jsx
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { ListGroupItem, ListGroup } from 'reactstrap';
+import { ListGroupItem, ListGroup, Badge } from 'reactstrap';
 import { bindActionCreators } from 'redux';
 import * as categoryActions from '../../redux/actions/categoryActions';
+import * as productActions from '../../redux/actions/productActions';
 
 class CategoryList extends Component {
     componentDidMount() {
         this.props.actions.getCategories();
     }
 
+    selectCategory = (category) => {
+        this.props.actions.changeCategory(category);
+        this.props.actions.getProducts(category.id);
+    };
+
     render() {
         return (
             <div>
-                <h3>Categories</h3>
+                <h3>
+                    <Badge color="warning">Categories</Badge>
+                </h3>
                 <ListGroup>
                     {this.props.categories.map((category) => (
-                        <ListGroupItem key={category.id}>
+                        <ListGroupItem
+                            active={
+                                category.id === this.props.currentCategory.id
+                            }
+                            onClick={() => this.selectCategory(category)}
+                            key={category.id}
+                        >
                             {category.categoryName}
                         </ListGroupItem>
                     ))}
@@ -40,6 +54,14 @@ function mapDispatchToProps(dispatch) {
         actions: {
             getCategories: bindActionCreators(
                 categoryActions.getCategories,
+                dispatch
+            ),
+            changeCategory: bindActionCreators(
+                categoryActions.changeCategory,
+                dispatch
+            ),
+            getProducts: bindActionCreators(
+                productActions.getProducts,
                 dispatch
             ),
         },
--- learning/redux-northwind/src/components/products/ProductList.jsx
+++ learning/redux-northwind/src/components/products/ProductList.jsx
@@ -1,7 +1,67 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as productActions from '../../redux/actions/productActions';
+import { Table, Button } from 'reactstrap';
 
-export default class ProductList extends Component {
+class ProductList extends Component {
+    componentDidMount() {
+        this.props.actions.getProducts();
+    }
+
     render() {
-        return <div>Product List</div>;
+        return (
+            <div>
+                <h2>
+                    Products-
+                    <span>{this.props.currentCategory.categoryName}</span>
+                </h2>
+                <Table striped>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Product Name</th>
+                            <th>Unit Price</th>
+                            <th>Quantity Per Unit</th>
+                            <th>Units In Stock</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.products.map((product) => (
+                            <tr key={product.id}>
+                                <td>{product.id}</td>
+                                <td>{product.productName}</td>
+                                <td>{product.unitPrice}</td>
+                                <td>{product.quantityPerUnit}</td>
+                                <td>{product.unitsInStock}</td>
+                                <td>
+                                    <Button color="danger">buy</Button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </div>
+        );
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        currentCategory: state.changeCategoryReducer,
+        products: state.productListReducer,
+    };
+}
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: {
+            getProducts: bindActionCreators(
+                productActions.getProducts,
+                dispatch
+            ),
+        },
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductList);
